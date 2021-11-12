3364a6563404fcbb55d41902d9c0da623448fd95
Jonathan <mail@jshimwell.com>
2021-11-12 21:17:50 +0000

    corrected dose units

--- openmc_tally_unit_converter/__init__.py
+++ openmc_tally_unit_converter/__init__.py
@@ -1,6 +1,6 @@
 from .utils import (
     find_fusion_energy_per_reaction,
-    get_tally_units,
+    get_score_units,
     check_for_dimentionality_difference,
     find_source_strength,
     compute_volume_of_voxels,
