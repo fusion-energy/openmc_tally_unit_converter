3649bd72acc0e741c3fbdba738190e713b822967
Jonathan Shimwell <mail@jshimwell.com>
2021-11-17 15:43:50 +0000

    added particle to workaround issue 39

--- tests/test_cell_effective_dose.py
+++ tests/test_cell_effective_dose.py
@@ -24,7 +24,7 @@ class TestUsage(unittest.TestCase):
         )
 
         assert len(result) == 1
-        assert result[0].units == "picosievert / source_particle"
+        assert result[0].units == "neutron * picosievert / source_particle"
 
     def test_cell_tally_dose_base_units(self):
         # returns the tally with base units
